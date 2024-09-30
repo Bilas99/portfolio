d31cf6430b5eebee366b25e5a68e86168674a3ed
Bilas Singha <bilassingha97@gamil.com>
2024-09-30 22:55:12 +0530

    add readme and fix minor bugs

--- README.md
+++ README.md
@@ -1,8 +1,86 @@
-# React + Vite
+# 🚀 **Personal Portfolio**
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Welcome to my personal portfolio built using 
+**React** with **Vite**! This project showcases 
+my skills and experience as a frontend developer. 
+It features smooth animations, dynamic interactions, 
+and a clean, modern design. Check it out:
 
-Currently, two official plugins are available:
+### [🔗 Preview Live](https://portfolio-ten-orpin-17.vercel.app/)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+---
+
+## ✨ **Features**
+
+- 🎨 **Beautiful Animations**: Implemented with <span style="color: #1ac7ed;">`framer-motion`</span> for smooth motion graphics and transitions.
+
+- ⌨️ **Typing Effects**: Achieved using <span style="color: #1ac7ed;">`react-typed`</span> to add engaging text animations.
+
+- 🎯 **Web Icons**: Integrated with <span style="color: #1ac7ed;">`react-icons`</span> to enhance visual design.
+
+- 📝 **Form Handling**: Utilized <span style="color: #1ac7ed;">`react-hook-form`</span> for efficient form management and validation.
+
+- 🔥 **Firebase Integration**: Stored project data and contact form submissions in Firebase for seamless database management.
+
+- 📚 **Dynamic Project Showcase**: Fetched and displayed project data directly from the Firebase database.
+
+---
+
+## 📦 **Project Setup**
+
+1. **Clone the repository:**
+
+   ```bash
+      git clone https://github.com/Bilas99/portfolio.git
+   ```
+
+2. **Navigate to the project directory:**
+
+   ````bash
+      cd portfolio
+   ````
+
+3. **Install dependencies:**
+
+   ````bash
+      npm install
+   ````
+
+4. **Start the development server:**
+
+   ```bash
+      npm run dev
+   ```
+
+---
+
+## 🛠️ **Tech Stack Used**
+
+   - **Frontend**: React with Vite
+   
+   - **Animations**: framer-motion
+   - **Icons**: react-icons
+   - **Typing Effect**: react-typed
+   - **Form Handling**: react-hook-form
+   - **Database**: Firebase Firestore
+   - **Hosting**: Vercel
+
+---
+
+## 🎉 **Explore More**
+
+   - Visit the [Live Preview](https://portfolio-ten-orpin-17.vercel.app/)
+   
+   - Check out the Code: [GitHub Repository](https://github.com/Bilas99/portfolio)
+
+---
+
+## 🙏 **Acknowledgments**
+   - Special thanks to all the libraries and tools that made this project possible.
+   - Big shoutout to the open-source community for their contributions and support!
+
+---
+
+## ❤️ **Made with Passion by** [Bilas Singha](https://github.com/Bilas99/)
+
+---
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -10,8 +10,8 @@ const Projects = () => {
   const [projects, setProjects] = useState([]);
   const [isError, setIsError] = useState(false);
   const [loading, setLoading] = useState(false);
-
-  // Fetch projects data from Firestore
+  
+   // Fetch projects data from Firestore
    useEffect(() => {
       ( async () => {
          try {
@@ -22,7 +22,7 @@ const Projects = () => {
                ...doc.data(),
             }));
             if(projectsList.length < 1) setIsError(true)
-            setProjects(projectsList);
+            setProjects(projectsList.sort((a, b) => (b.new ? 1 : 0) - (a.new ? 1 : 0)))
          } catch (e) {
             setIsError(true)
          } finally {
@@ -30,10 +30,6 @@ const Projects = () => {
          }
       })();
   }, []);
-  
-   const handleReload = () => {
-      window.navigation.reload()
-   }
   
    const fadeInUp = {
       hidden: { opacity: 0, y: 50 },
@@ -55,62 +51,84 @@ return (
          <div className="grid min-h-[500px] md:grid-cols-3 place-items-center gap-4 my-6">
             {/* Loop Projects */}
             { projects.map((project) => (
-            <div key={project.id} className="w-full p-3 bg-gray-200 dark:bg-slate-800 shadow-md rounded-lg">
+            <div 
+               key={project.id} 
+               className="w-full min-h-[420px] p-3 relative bg-gray-200 dark:bg-slate-800 shadow-md rounded-lg overflow-hidden"
+            >
                <img 
                   src={project.image} 
                   alt={project.title} 
                   className="w-full h-64 bg-cover rounded-lg"
                />
-               <div className="mt-2 flex flex-col justify-between items-stretch">
-                  <h2 className="text-xl font-montserrat font-extrabold text-[#1ac7ed]">
-                     {project.title}
-                  </h2>
-                  <div className="mb-2 text-slate-800 dark:text-gray-300">
-                     <h3 className="text-md text-[#9d8622] font-montserrat font-bold">
-                        Tools:
-                     </h3>
-                     <p className="text-sm font-medium">
-                        {project.tools}
-                     </p>
+               <div className="mt-2 min-h-[136px] flex flex-col justify-between items-center">
+                  {/* texts section */}
+                  <div className="w-full h-auto">   
+                     <h2 className="text-xl font-montserrat font-extrabold text-[#1ac7ed]">
+                        {project.title}
+                     </h2>
+                     <div className="mb-2 text-slate-800 dark:text-gray-300">
+                        <h3 className="text-md text-[#9d8622] font-montserrat font-bold">
+                           Tools:
+                        </h3>
+                        <p className="text-sm line-clamp-2 font-medium">
+                           {project.tools}
+                        </p>
+                     </div>
                   </div>
-                  <ul className="mb-0 flex items-center gap-4">
-                     <li className="w-full font-medium text-center hover:scale-95 hover:opacity-90 duration-100">
-                        <Link to={project.url}
-                           target="_blank"
-                           className="block py-1.5 bg-[#9d8622] text-slate-900 rounded-lg"
-                        >
-                           Preview
-                        </Link>
-                     </li>
-                     <li className="hover:scale-95 duration-100">
-                        <Link to={project.github}
-                           target="_blank"
-                           className="text-3xl dark:text-white"
-                        >
-                           <FaGithub />
-                        </Link>
-                     </li>
-                  </ul>
+                  
+                  {/* buttons section */}
+                  <div className="w-full h-auto">
+                     <ul className="mb-0 flex items-center gap-4">
+                        <li className="w-full font-medium text-center hover:scale-95 hover:opacity-90 duration-100">
+                           <Link to={project.url}
+                              target={project.target || "_self"}
+                              className="block py-1.5 bg-[#9d8622] text-slate-900 rounded-lg"
+                           >
+                              Preview
+                           </Link>
+                        </li>
+                        <li className="hover:scale-95 duration-100">
+                           <Link to={project.github}
+                              target="_blank"
+                              className="text-3xl dark:text-white"
+                           >
+                              <FaGithub />
+                           </Link>
+                        </li>
+                     </ul>
+                  </div>
                </div>
+               
+               {/* new badge */}
+               { project.new && (
+                  <div className="w-20 py-1 text-sm font-bold text-center bg-[#e3ba31] absolute top-2 -left-6 -rotate-45 -skew-y-6">
+                     New
+                  </div> )
+               }
             </div>))}
             
             {/* when Loading */}
             { loading && (
-               <div class="w-16 h-16 col-span-3 border-4 border-white border-t-black dark:border-black dark:border-t-white animate-spin rounded-full">
+               <div class="w-16 h-16 col-span-3 border-4 border-gray-300 border-t-black dark:border-slate-800 dark:border-t-white animate-spin rounded-full">
                </div> )
             }
             
             {/* when Error */}
             { isError && (
-            <div>
-               <div class="col-span-3 text-2xl text-center text-slate-700 dark:text-gray-400 font-extrabold">
-                  Sorry! Error Fetching Projects.
+            <div className="col-span-3 bg-amber-100 text-center">
+               <div className="text-slate-700 dark:text-gray-400">
+                  <h1 className="text-2xl font-extrabold">
+                     Sorry! Error Fetching Projects.
+                  </h1>
+                  <h2 className="text-lg font-medium">
+                     Please try again!
+                  </h2>
                </div>
                <div 
-                  onClick={handleReload} 
-                  className="my-6 mx-auto w-max py-1 px-2 font-medium rounded shadow-md text-slate-700 bg-gray-200 dark:text-gray-400 dark:bg-slate-800"
+                  onClick={() => window.location.reload()} 
+                  className="my-6 mx-auto w-max py-1 px-2 font-medium rounded shadow-md text-[#1ac7ed] bg-gray-200 dark:bg-slate-800"
                >
-                  Relod
+                  Reload
                </div>
             </div>
             )}
